d387afa6c0938390ba9fbcfb1449efa1d1fce9fb
Harnoor82 <HARNOOR.26011@stu.upes.ac.in>
2026-09-21 16:24:37 +0530

    week-1

--- Q-5.c
+++ Q-5.c
@@ -4,7 +4,7 @@
 
 int main() {
     int a[10][10], b[10][10], c[10][10];
-    int r1, c1, r2, c2, i, j, k;
+    int r1, c1, r2, c2, i, j, k; 
 
     printf("Enter rows and columns of first matrix: ");
     scanf("%d %d", &r1, &c1);

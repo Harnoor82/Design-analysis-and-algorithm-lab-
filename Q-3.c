bb989525adb8fd10a9acb72ff29888a184812549
Harnoor82 <HARNOOR.26011@stu.upes.ac.in>
2026-09-21 16:19:44 +0530

    Day-3

--- Q-3.c
+++ Q-3.c
@@ -1,5 +1,4 @@
-//Write a program to merge two sorted arrays into a single sorted array 
-//without using any built-in sorting function.
+//Q-3 Write a program to merge two sorted arrays into a single sorted array , without using any built-in sorting function.
 
 #include <stdio.h>
 
